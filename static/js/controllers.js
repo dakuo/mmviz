9723c6750042150090ca82ff5bae0586f5afd022
Dakuo Wang <Dakuo.Wang@ibm.com>
2018-07-16 05:32:03 +0000

    test 1, new Viz page and changed Route

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -3,11 +3,71 @@ var ConsoleModule = angular.module('ConsoleModule', ['ngRoute']);
 
 ConsoleModule.config(['$routeProvider', '$locationProvider','$sceDelegateProvider', '$httpProvider',
     function ($routeProvider, $locationProvider, $sceDelegateProvider, $httpProvider) {
-    $routeProvider.when('/', {
+    $routeProvider.when('/weather', {
         templateUrl: '/partials/Byzip.html',
         controller: 'wcontroller',
         controllerAs: 'wcontroller'
     });
+    
+    $routeProvider.when('/', {
+        templateUrl: '/partials/Viz.html',
+        controller: 'vcontroller',
+        controllerAs: 'vcontroller'
+    });
+}]);
+
+ConsoleModule.controller('vcontroller', ['$scope', '$http', '$routeParams', '$timeout', '$sce',
+    function($scope, $http, $routeParams, $timeout, $sce) {
+
+    $scope.somemessage = "Some weather";
+    $scope.zip1City = "";
+    $scope.zip1Weather = "";
+
+    $scope.zip = function(which) {
+
+        var data = "";
+        if(which === 1) {
+            data = $scope.zip1m;
+        } else if(which === 2) {
+            data = $scope.zip2m;
+        }
+
+        if(data.length === 5) {
+            $http({
+                method: "GET",
+                url: '/api/v1/getWeather?zip=' + data
+            }).then( function(response) {
+                if(which === 1) {
+                    $scope.zip1City = response.data.city;
+                    $scope.zip1Weather = response.data.weather;
+                } else if(which === 2) {
+                    $scope.zip2City = response.data.city;
+                    $scope.zip2Weather = response.data.weather;
+                } else if(which === 3) {
+                    $scope.zip3City = response.data.city;
+                    $scope.zip3Weather = response.data.weather;
+                } else if(which === 4) {
+                    $scope.zip4City = response.data.city;
+                    $scope.zip4Weather = response.data.weather;
+                } 
+            });
+        } else {
+            if(which === 1) {
+                    $scope.zip1City = "";
+                    $scope.zip1Weather = "";
+                } else if(which === 2) {
+                    $scope.zip2City = "";
+                    $scope.zip2Weather = "";
+                } else if(which === 3) {
+                    $scope.zip3City = "";
+                    $scope.zip3Weather = "";
+                } else if(which === 4) {
+                    $scope.zip4City = "";
+                    $scope.zip4Weather = "";
+                } 
+        }
+    };
+    
 }]);
 
 ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$timeout', '$sce',
